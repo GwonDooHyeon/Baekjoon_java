9a5a05c91eb614b0cb96e170ea4b6c47eaab32e2
GwonDooHyeon <secure3141@gmail.com>
2024-06-09 22:50:30 +0900

    String(문자열) - 7.회문 문자열

---
+++ src/문자열/InfLearn_01_07.java
@@ -0,0 +1,23 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://cote.inflearn.com/contest/10/problem/01-07
+// 회문 문자열
+public class InfLearn_01_07 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        str = str.toUpperCase();
+        StringBuilder sb = new StringBuilder(str);
+
+        if (str.contentEquals(sb.reverse())) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
