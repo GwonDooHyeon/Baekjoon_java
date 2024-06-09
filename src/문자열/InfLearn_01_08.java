c1c1c0a62415cf7405b3b6bbea902add12a5e2e7
GwonDooHyeon <secure3141@gmail.com>
2024-06-10 00:22:52 +0900

    String(문자열) - 8.유효한 팰린드롬

---
+++ src/문자열/InfLearn_01_08.java
@@ -0,0 +1,23 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://cote.inflearn.com/contest/10/problem/01-08
+// 유효한 팰린드롬
+public class InfLearn_01_08 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        str = str.toUpperCase().replaceAll("[^a-zA-Z]", "");
+        StringBuilder sb = new StringBuilder(str);
+
+        if (str.contentEquals(sb.reverse())) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
