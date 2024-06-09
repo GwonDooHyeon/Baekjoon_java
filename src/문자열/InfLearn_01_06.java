5a1500468a1a8145454686fd78739754513b8885
GwonDooHyeon <secure3141@gmail.com>
2024-06-09 19:14:33 +0900

    String(문자열) - 6.중복문자제거

---
+++ src/문자열/InfLearn_01_06.java
@@ -0,0 +1,27 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+// https://cote.inflearn.com/contest/10/problem/01-06
+// 중복 문자제거
+public class InfLearn_01_06 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        Set<Character> set = new LinkedHashSet<>();
+
+        for (char c : str.toCharArray()) {
+            set.add(c);
+        }
+        for (Character c : set) {
+            sb.append(c);
+        }
+
+        System.out.println(sb);
+    }
+}
