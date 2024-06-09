fb62c85454863bb08adc8306b47948dfb74803ef
GwonDooHyeon <secure3141@gmail.com>
2024-06-09 10:55:34 +0900

    String(문자열) - 1.문자 찾기

---
+++ src/문자열/InfLearn_01_01.java
@@ -0,0 +1,25 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://cote.inflearn.com/contest/10/problem/01-01
+// 문자 찾기
+public class InfLearn_01_01 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine().toUpperCase();
+        char c = Character.toUpperCase(br.readLine().charAt(0));
+        int count = 0;
+
+        for (char x : str.toCharArray()) {
+            if (x == c) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
