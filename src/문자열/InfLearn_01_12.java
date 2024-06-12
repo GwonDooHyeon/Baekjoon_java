be69a8c9928fc5f20710b0cd2a9cbb746dec0658
GwonDooHyeon <secure3141@gmail.com>
2024-06-13 06:36:16 +0900

    String(문자열) - 12.암호

---
+++ src/문자열/InfLearn_01_12.java
@@ -0,0 +1,27 @@
+package 문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://cote.inflearn.com/contest/10/problem/01-12
+// 암호
+public class InfLearn_01_12 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 입력
+        int n = Integer.parseInt(br.readLine()); // 4
+        String str = br.readLine(); // #****###**#####**#####**##**
+        String tmp = "";
+        
+        for (int i = 0; i < n; i++) {
+            tmp = str.substring(0, 7).replace("#", "1").replace("*", "0");
+            int answer = Integer.parseInt(tmp, 2);
+            sb.append((char) answer);
+            str = str.substring(7);
+        }
+        System.out.println(sb);
+    }
+}
