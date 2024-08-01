78dbda60cb62bd9ed8ddac75c14356243b0bc9ec
GwonDooHyeon <secure3141@gmail.com>
2024-08-01 23:33:02 +0900

    스택 - 제로

---
+++ src/스택_큐_덱/BOJ_10773.java
@@ -0,0 +1,31 @@
+package 스택_큐_덱;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// https://www.acmicpc.net/problem/28278
+// 제로
+public class BOJ_10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        for (int num : stack) {
+            sum += num;
+        }
+        System.out.println(sum);
+    }
+}
